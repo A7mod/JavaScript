24ff380f1482c91499aabc21083790e6540d9982
Aamod Krishna <31404112+A7mod@users.noreply.github.com>
2023-06-10 23:48:24 +0530

    added eventListeners for these buttons. f* stupid

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -41,5 +41,8 @@ function divide(){
 }
 
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
 
 

9c546df55ea3a453c49be962303e76c626d5beac
Aamod Krishna <31404112+A7mod@users.noreply.github.com>
2023-06-11 23:01:15 +0530

    this is some string items being thrown here

--- self-understanding/code_academy/one.js
+++ self-understanding/code_academy/one.js
@@ -1,2 +1,7 @@
 const entree = 'Enchiladas';
-console.log(entree);
+console.log('This is tres tres best ' + entree);
+
+// interpolation in place of concatenation
+
+const myPet = 'Fat Panda';
+console.log(`I own a pet ${myPet}.`);   // ${item for output} 

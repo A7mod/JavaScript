81cd8b9be1011ece8cc54dc7c4f6d95116031026
Aamod Krishna <31404112+A7mod@users.noreply.github.com>
2023-06-12 14:40:35 +0530

    Just a little Rage

---
+++ self-understanding/code_academy/conditionals.js
@@ -0,0 +1,12 @@
+if(true){
+    console.log("This gonna print");
+
+}
+
+let sale = true;
+if(sale){
+    console.log("interesting ideas")
+}
+
+
+// very basic basic conditionals here man, I hate doing it again and again!! all over again

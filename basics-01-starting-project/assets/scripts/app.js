7b1e03e0ed07b1fc8295ed5d171ec8c61b170684
Aamod Krishna <31404112+A7mod@users.noreply.github.com>
2023-06-13 14:47:01 +0530

    adding Logging, interesting but here is stupid

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,5 +1,6 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 
 function getUserNumberInput(){
     return parseInt(userInput.value);
@@ -12,12 +13,27 @@ function createWriteOutput(operator, resultBefore, calcNumber){
 
 }
 
+function writeToLog(opId, prevResult, opNum, newResult) {
+    const logEntry = {
+        operation : opId,
+        prevResult : prevResult,
+        number : opNum,
+        result : newResult
+    };
+    logEntries.push(logEntry);
+    console.log(logEntries);
+
+}
+
 function add(){
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
     currentResult += enteredNumber;
     createWriteOutput('+', initialResult, enteredNumber);
-}
+    
+
+    }
+
 
 function subtract(){
     const enteredNumber = getUserNumberInput();

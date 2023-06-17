f74c8d5f9b216c3209a7d0b1751d25585c40b01b
Aamod Krishna <31404112+A7mod@users.noreply.github.com>
2023-06-17 12:00:19 +0530

    this anonymous

---
+++ self-understanding/code_academy/functions2.js
@@ -0,0 +1,8 @@
+// a peculiar type of function called an anonymous function. 
+
+const ironHeart = function(iron, heart){
+    const mul = iron * heart * 20;
+    return mul;
+}
+
+console.log(ironHeart(12, 25));

1344dd17b46deca584d0c3c0127021361923e6b1
Aamod Krishna <31404112+A7mod@users.noreply.github.com>
2023-06-17 11:28:37 +0530

    default parameters etc

---
+++ self-understanding/code_academy/functions.js
@@ -0,0 +1,8 @@
+// let's just dig in default parameters
+function powerMove(item1 = 'Interesting', twice = 900, power = 'Less Interesting'){
+    console.log(`Nice way to be ${item1}`);
+    console.log(`Nice way to be ${twice}`);
+    console.log(`Nice way to be ${power}`);
+}
+
+powerMove();

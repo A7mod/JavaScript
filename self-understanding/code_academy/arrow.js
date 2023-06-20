7c0fc6eaa7a637952621839b1670edefa3e1f043
Aamod Krishna <31404112+A7mod@users.noreply.github.com>
2023-06-20 19:29:47 +0530

    arrow fns, concise body

---
+++ self-understanding/code_academy/arrow.js
@@ -0,0 +1,13 @@
+// this is a new type of thing
+// there are arrow functions, yes I know that. 
+// these are called concise body
+
+/*
+const plantNeedsWater = (day) => {
+ return day === 'Wednesday' ? true : false;
+};
+
+plantNeedsWater();
+*/
+
+const plantNeedsWater = day => day === 'Wednesday' ? true : false;

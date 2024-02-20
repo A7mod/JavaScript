12502d025325eec84ac44d7e6891ac3975b195ed
Aamod Krishna <31404112+A7mod@users.noreply.github.com>
2024-02-20 23:11:15 +0530

    changing a lot of code, don't ask

--- self-understanding/30days/callback/script2.js
+++ self-understanding/30days/callback/script2.js
@@ -1,22 +1,28 @@
 // callback hell
 
-function loadingData(callback) {
-  setTimeout(() => {
-    console.log("1) Loading Data ....");
-    callback();
-  }, 4000); // this is taking 4 secs
+function loadingData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("1) Loading Data ....");
+      resolve();
+    }, 2000); // this is taking 4 secs
+  });
 }
-function collectingData(callback) {
-  setTimeout(() => {
-    console.log("2) Collecting Data ....");
-    callback();
-  }, 5000); // while this one is taking 5 seconds after the 4 secs
+function collectingData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("2) Collecting Data ....");
+      resolve();
+    }, 3000); // while this one is taking 5 seconds after the 4 secs
+  });
 }
-function approvingData(callback) {
-  setTimeout(() => {
-    console.log("3) Approving Data ....");
-    callback();
-  }, 3000);
+function approvingData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("3) Approving Data ....");
+      resolve();
+    }, 3000);
+  });
 }
 function approved() {
   console.log("4) Approved!");
@@ -24,12 +30,15 @@ function approved() {
 
 // callback hell
 
-loadingData(function () {
-  collectingData(function () {
-    approvingData(function () {
-      approved();
-    });
-  });
-});
+// loadingData(function () {
+//   collectingData(function () {
+//     approvingData(function () {
+//       approved();
+//     });
+//   });
+// });
 
 // we use promises to get away from this hell
+
+// calling those functions with promises
+loadingData().then(collectingData).then(approvingData);

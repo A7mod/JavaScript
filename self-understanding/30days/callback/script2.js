3e7b99b611440926de6337930096927e4ee73317
Aamod Krishna <31404112+A7mod@users.noreply.github.com>
2024-02-20 22:50:20 +0530

    callback hell understood

--- self-understanding/30days/callback/script2.js
+++ self-understanding/30days/callback/script2.js
@@ -6,20 +6,30 @@ function loadingData(callback) {
     callback();
   }, 4000); // this is taking 4 secs
 }
-function collectingData() {
-  console.log("2) Collecting Data ....");
+function collectingData(callback) {
+  setTimeout(() => {
+    console.log("2) Collecting Data ....");
+    callback();
+  }, 5000); // while this one is taking 5 seconds after the 4 secs
 }
-function approvingData() {
-  console.log("3) Approving Data ....");
+function approvingData(callback) {
+  setTimeout(() => {
+    console.log("3) Approving Data ....");
+    callback();
+  }, 3000);
 }
 function approved() {
   console.log("4) Approved!");
 }
 
+// callback hell
+
 loadingData(function () {
-  collectingData();
-
-  approvingData();
-
-  approved();
+  collectingData(function () {
+    approvingData(function () {
+      approved();
+    });
+  });
 });
+
+// we use promises to get away from this hell

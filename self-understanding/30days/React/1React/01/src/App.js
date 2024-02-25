20484e76f4c648b41c4bebe610c39027f3e7ba4f
Aamod Krishna <31404112+A7mod@users.noreply.github.com>
2024-02-25 19:54:32 +0530

    empty commits

--- self-understanding/30days/React/1React/01/src/App.js
+++ self-understanding/30days/React/1React/01/src/App.js
@@ -1,7 +1,9 @@
 function App() {
   // how to assign values? Use JSX.
+  let a = 2;
   return (
     <div>
+      {a}
       <h1>Hello, I am Component. </h1>
       <p>This is Tutorial of React. </p>;
     </div>
